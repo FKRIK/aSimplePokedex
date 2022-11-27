037785745d06047e019e709a8a3deb5ea8680b59
FKRIK <fernandokrik@hotmail.com>
2022-11-26 22:44:45 -0300

    added image on details

--- app/src/main/java/br/com/up/pokedex/MainActivity.kt
+++ app/src/main/java/br/com/up/pokedex/MainActivity.kt
@@ -19,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        supportActionBar!!.hide()
 
         val recyclerView : RecyclerView
             = findViewById(R.id.poke_recycler)
--- app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
+++ app/src/main/java/br/com/up/pokedex/PokemonDetailsActivity.kt
@@ -2,7 +2,10 @@ package br.com.up.pokedex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
 import br.com.up.pokedex.network.PokeApi
+import com.squareup.picasso.Picasso
 
 class PokemonDetailsActivity : AppCompatActivity() {
 
@@ -11,6 +14,15 @@ class PokemonDetailsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_pokemon_details)
 
         val pokemon = intent.getStringExtra("pokemon")
+
+        val pokemonNome : TextView = findViewById(R.id.txt_pokemon_name)
+        val pokemonImagem : ImageView = findViewById(R.id.img_pokemon)
+        val pokemonTipo : TextView = findViewById(R.id.txt_pokemon_tipo)
+        val pokemonMov : TextView = findViewById(R.id.txt_pokemon_mov)
+        val pokemonHab : TextView = findViewById(R.id.txt_pokemon_hab)
+
+        val url = "https://pokeapi.co/api/v2/pokemon/${pokemon}"
+        Picasso.get().load(url).into(pokemonImagem)
 
         PokeApi().getPokemonByName(pokemon!!){
                 pokemon ->
